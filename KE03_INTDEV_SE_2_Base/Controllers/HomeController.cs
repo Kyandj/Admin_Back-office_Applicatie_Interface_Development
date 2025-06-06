db346eb813ff63d469b93795d3422594c177ce72
Divancia-ICT <2403974ananda@zuyd.nl>
2025-06-06 08:44:45 +0200

    heb de alle producten pagina ook mooier gemaakt

--- KE03_INTDEV_SE_2_Base/Controllers/HomeController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/HomeController.cs
@@ -1,20 +1,32 @@
 using System.Diagnostics;
+using DataAccessLayer;
 using KE03_INTDEV_SE_2_Base.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace KE03_INTDEV_SE_2_Base.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MatrixIncDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MatrixIncDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            int lowStockThreshold = 5;
+            var lowStockProducts = await _context.Products
+                .Where(p => p.Stock <= lowStockThreshold)
+                .OrderBy(p => p.Stock)
+                .ToListAsync();
+
+            ViewBag.LowStockProducts = lowStockProducts;
             return View();
         }
 
